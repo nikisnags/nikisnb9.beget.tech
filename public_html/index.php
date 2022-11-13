e8a26f68e58e470ee40f6b548a64b6f618a5f66d
Roman Novikov <47324027+cyber-roNO@users.noreply.github.com>
2022-11-13 16:35:28 +0300

    вынес пхп логику в head

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,24 @@
         <meta charset="UTF-8">
         <title>Sinabons Team</title>
         <link rel="stylesheet" href="assets/css/main.css">
+        <?php
+            $db_host="localhost";
+            $db_name="nikisnb9_1"; 
+            $db_user="nikisnb9_1"; 
+            $db_pass="nikisnb9_1pass"; 
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+            $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name); 
+            $mysqli->set_charset("utf8mb4"); 
+            $result = $mysqli->query("SELECT * FROM `table1` ORDER BY id DESC"); 
+            $result2 = $mysqli->query("SELECT count(*) FROM table1");
+            $row2 = $result2->fetch_row();
+            $count = $row2[0];
+            $i = $count;
+            $result3 = $mysqli->query("SELECT time1 FROM table1 ORDER BY id DESC LIMIT 1");
+            $time1 = $result3->fetch_row();
+            $old = $time1[0];
+            $old1 = date("d.m.Y",strtotime($old));
+        ?>
     </head>
     <body>
         <main>
@@ -11,24 +29,7 @@
                 <img src="assets/imgs/back.png" alt="bg">
             </figure>
             <div class="main__chat">
-                <?php
-                    $db_host="localhost";
-                    $db_name="nikisnb9_1"; 
-                    $db_user="nikisnb9_1"; 
-                    $db_pass="nikisnb9_1pass"; 
-                    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-                    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name); 
-                    $mysqli->set_charset("utf8mb4"); 
-                    $result = $mysqli->query("SELECT * FROM `table1` ORDER BY id DESC"); 
-                    $result2 = $mysqli->query("SELECT count(*) FROM table1");
-                    $row2 = $result2->fetch_row();
-                    $count = $row2[0];
-                    $i = $count;
-                    $result3 = $mysqli->query("SELECT time1 FROM table1 ORDER BY id DESC LIMIT 1");
-                    $time1 = $result3->fetch_row();
-                    $old = $time1[0];
-                    $old1 = date("d.m.Y",strtotime($old));
-                    
+               <?
                     while($row = $result->fetch_assoc())
                     {
                         $date = $row[time1];
